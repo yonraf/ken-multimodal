3c00da363ce1405ff7c35b8990008c3fffa43461
yonraf <moraf19@student.sdu.dk>
2024-03-25 15:37:18 +0100

    Applied handling of commands

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,7 @@ import requests
 import json
 from flask_cors import CORS
 from faster_whisper import WhisperModel
-
+import re
 
 
 app = Flask(__name__)
@@ -35,7 +35,65 @@ def transcribe():
     response = requests.post(url, headers=headers, json=data)
     result = json.loads(response.text)["response"]
     print('Response from Ollama:\n',result)
+    handle_command(result)
     return result
 
+# ----- COMMAND HANDLER -----
+
+# Regular expression pattern to match command structures
+pattern = r"\((\w+), (\w+)(?:, (\w+))?\)"
+
+def handle_wash(command):
+    print("Sending Rack", command[1]," to wash")
+    #url = f"http://172.22.102.39:3000/createJob?rackId={rack_id}&jobType=1"    
+    #response = request.post(url)
+
+def handle_move(command):
+    print("Moving rack ", command[1], " to location ", command[2])
+    
+    # need to convert both rack_id and position_id
+    #url = f"http://172.22.102.39:3000/createJob?rackId={rack_id}&jobType=100&dropoffPositionId={position_id}"
+    #response = request.post(url)
+
+def handle_return(command):
+    print("Returning rack ", command[1])
+    # FETCH ID OF RACK
+    #url = f"http://172.22.102.39:3000/createJob?rackId={rack_id}&jobType=7"    
+    #response = request.post(url)
+
+def handle_safe(command):
+    print("Moving Robot ", command[1]," to safe position")
+
+def handle_cancel(command):
+    print("Aborting job with rack ", command[1])
+
+def handle_none(command):
+    print("It does not seem like you intend to perform a task")
+
+# Dictionary mapping command types to handler functions
+command_handlers = {
+    "wash": handle_wash,
+    "move": handle_move,
+    "return": handle_return,
+    "safe": handle_safe,
+    "cancel": handle_cancel,
+    "none": handle_none
+}
+
+def handle_command(command):
+    # Find command in the input string
+    match = re.search(pattern, command.lower())
+
+    if match:
+        command = match.groups()
+        action = command[0]
+        handler = command_handlers.get(action)
+        if handler:
+            handler(command)
+        else:
+            print("No handler found for command:", action)
+    else:
+        print("No valid command found in the input string.")
+    
 if __name__ == '__main__':
     app.run(port=5000)
